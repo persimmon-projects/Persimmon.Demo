d4754432dc6e4134132036edae40c686b30b1163
pocketberserker <pocketberserker@gmail.com>
2014-12-10 23:20:37 +0900

    add divide test

---
+++ Persimmon.Demo/DivideTest.fs
@@ -0,0 +1,17 @@
+﻿namespace Persimmon.Demo
+
+open Persimmon
+open System
+
+module DivideTest =
+
+  let divide x y = x / y
+
+  let ``simple test example`` = test "simple test exmple" {
+    do! assertEquals 2 (divide 5 2)
+  }
+
+  let ``exception test example`` = test "exception test example" {
+    let! e = trap { it (divide 3 0) }
+    do! assertEquals  typeof<ArithmeticException> (e.GetType())
+  }
